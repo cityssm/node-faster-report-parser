14c5b1dde627b7d66e37f533ddc10df7603d1bb9
Dan Gowans <dangowans@users.noreply.github.com>
2024-11-04 10:42:02 -0500

    disable no-magic-numbers rule

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,1 +1,9 @@
-export { default } from 'eslint-config-cityssm'
+import cityssmConfig, { tseslint } from 'eslint-config-cityssm'
+
+export const config = tseslint.config(...cityssmConfig, {
+  rules: {
+    '@typescript-eslint/no-magic-numbers': 'off'
+  }
+})
+
+export default config
