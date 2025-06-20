98cae45c214e8460aff469ffb429f32777a31276
Dan Gowans <dangowans@users.noreply.github.com>
2025-06-20 12:41:48 -0400

    version bump

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export * as advanced from './advanced.js';
 export * as csvReports from './csvReports.js';
 export * as xlsxReports from './xlsxReports.js';
+export * as xmlReports from './xmlReports.js';
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 export * as advanced from './advanced.js'
+
 export * as csvReports from './csvReports.js'
 export * as xlsxReports from './xlsxReports.js'
+export * as xmlReports from './xmlReports.js'
